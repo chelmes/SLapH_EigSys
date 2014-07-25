2b825bb4e83ab5cd833f9556c810641a0c72af0d
Christopher Helmes <helmes@hiskp.uni-bonn.de>
2014-07-25 10:27:37 +0200

    added parameter input

--- modules/parastruct.cpp
+++ modules/parastruct.cpp
@@ -8,9 +8,9 @@ void IO::set_values(const char* infile) {
   std::string value;
   
   //three map class instances for int double and const char
-  std::map <std::string, std::string> path;
-  std::map <std::string, int> integer;
-  std::map <std::string, double> floating;
+  std::map <std::string, std::string> names;
+  std::map <std::string, int> integers;
+  std::map <std::string, double> floatings;
 
   //stream for config file
   std::fstream config;
@@ -26,66 +26,67 @@ void IO::set_values(const char* infile) {
 
         //String valued map variables
         if (keyword == "config_path") {
-          path["config_path"] = value;
+          names["config_path"] = value;
         }
         if (keyword == "result_path") {
-          path["result_path"] = value;
+          names["result_path"] = value;
         }
 
         //Integer valued map variables
         if (keyword == "LT" ) {
-          integer["LT"] = atoi(value.c_str());
+          integers["LT"] = atoi(value.c_str());
         }
         if (keyword == "LX" ) {
-          integer["LX"] = atoi(value.c_str());
+          integers["LX"] = atoi(value.c_str());
         }
         if (keyword == "NEV") {
-          integer["NEV"] = atoi(value.c_str());
+          integers["NEV"] = atoi(value.c_str());
         }
         if (keyword == "iter") {
-          integer["iter"] = atoi(value.c_str());
+          integers["iter"] = atoi(value.c_str());
         }
 
         //floating point valued map variables
         if (keyword == "alpha_1") {
-          floating["alpha_1"] = atof(value.c_str());
+          floatings["alpha_1"] = atof(value.c_str());
         }
         if (keyword == "alpha_2") {
-          floating["alpha_2"] = atof(value.c_str());
+          floatings["alpha_2"] = atof(value.c_str());
         }
         if (keyword == "LAM_L") {
-          floating["LAM_L"] = atof(value.c_str());
+          floatings["LAM_L"] = atof(value.c_str());
         }
         if (keyword == "LAM_C") {
-          floating["LAM_C"] = atof(value.c_str());
+          floatings["LAM_C"] = atof(value.c_str());
         }
     }
   }
   config.close();
 
   //set the values
-  config_path = path["config_path"];
-  result_path = path["result_path"];
+  config_path = names["config_path"];
+  result_path = names["result_path"];
 
-  LT = integer["LT"];
-  LX = integer["LX"];
+  LT = integers["LT"];
+  LX = integers["LX"];
   LY=LX;
   LZ=LX;
   
-  NEV = integer["NEV"];
+  NEV = integers["NEV"];
   NCOL = 3;
   NDIR = 4;
   V3 = LX*LY*LZ;
-  
+  MAT_ENTRIES=V3*NCOL;
+
   V_TS = LX*LY*LZ*NDIR*NCOL*NCOL*2; //2 is for complex
   V4_LIME = V_TS * LT;
   
-  iter = integer["iter"];
-  alpha_1 = floating["alpha_1"];
-  alpha_2 = floating["alpha_2"];
+  iter = integers["iter"];
+  alpha_1 = floatings["alpha_1"];
+  alpha_2 = floatings["alpha_2"];
 
-  LAM_L = floating["LAM_L"];
-  LAM_C = floating["LAM_C"];
+  LAM_L = floatings["LAM_L"];
+  LAM_C = floatings["LAM_C"];
 
 }
 
@@ -135,6 +136,9 @@ int IO::get_int(std::string spec) {
 
   if (spec == "iter") {
     return iter;
+  }
+  if (spec == "MAT_ENTRIES") {
+    return MAT_ENTRIES;
   }
   else return -1;
 
