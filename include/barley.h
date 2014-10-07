e42323d5d75ee702169e3e3a25c2011a231339d6
Christopher Helmes <helmes@hiskp.uni-bonn.de>
2014-10-07 10:07:08 +0200

    updated stuff

--- include/barley.h
+++ include/barley.h
@@ -1,13 +1,15 @@
 #ifndef _BARLEY_H_
 #define _BARLEY_H_
 
-typedef std::complex<double> 
+#include <array>
 
-class Gaugefield {
+class Gaugeconf {
 
   public:
-
+    Gaugeconf();
+    ~Gaugeconf();
   private:
-    
+  //Memory for one complete configuration
+    std::array <double> gauge_cfg;
 };
 #endif //_BARLEY_H_
--- include/pottstill.h
+++ include/pottstill.h
@@ -1,3 +1,6 @@
+//Functions to read and write from and to files. Implemented as Cheshire Cat, We
+//are all mad here
+
 #ifndef _POTTSTILL_H_
 #define _POTTSTILL_H_
 
@@ -8,9 +11,20 @@ class DataIO {
   public:
     DataIO();
     ~DataIO();
-    void read();
+    // Reads in gauge configuration from lime format into array
+    virtual void read_gauge_cfg();
+    // writes the eigenvectors to binary format.
+    // Eigenvectors are written per timeslice ordered by their eigenvalues
+    // from lowest to NEVth eigenvalue
+    virtual void write_evecs();
+    // writes the eigenvalues to binary format
+    // same storage layout as for the eigenvectors
+    virtual void write_evals();
+    // Same story for the eigenvalues
+    virtual void write_phases();
+
   private:
     class read_write;
-    std::unique_ptr<read_write> pimpl;
+    std::unique_ptr<read_write> smile;
 };
 #endif //_POTTSTILL_H_
